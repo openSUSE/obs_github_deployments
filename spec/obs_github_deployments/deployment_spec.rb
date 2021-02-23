e0d8355904ec008cfc05cd752e04d27f19d7572e
Lukas Krause <lkrause@suse.de>
2021-02-23 22:47:35 +0100

    Create rspec test for locked? method

---
+++ spec/obs_github_deployments/deployment_spec.rb
@@ -0,0 +1,17 @@
+# frozen_string_literal: true
+
+require "test_helper"
+
+RSpec.describe ObsGithubDeployments::Deployment, :vcr do
+  subject { ObsGithubDeployments::Deployment.new(repository: gh_test_repository, access_token: gh_test_access_token) }
+
+  describe "locked?" do
+    it "returns true when deployment is locked" do
+      expect(subject.locked?).to eq(true)
+    end
+
+    it "returns false when deployment is not locked" do
+      expect(subject.locked?).to eq(false)
+    end
+  end
+end
