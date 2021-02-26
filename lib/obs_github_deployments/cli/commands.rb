e4d306cf7f4c8b2b3edce811e1d4f63e8202fa3e
Lukas Krause <lkrause@suse.de>
2021-02-26 18:18:49 +0100

    Create set-lock command

--- lib/obs_github_deployments/cli/commands.rb
+++ lib/obs_github_deployments/cli/commands.rb
@@ -6,6 +6,7 @@ module ObsGithubDeployments
       extend Dry::CLI::Registry
       # register the commands and its command line
       register "version", Version, aliases: ["v", "-v", "--version"]
+      register "set-lock", SetLock, aliases: ["s", "-s", "--set-lock"]
     end
   end
 end
